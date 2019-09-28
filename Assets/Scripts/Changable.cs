fab6dfa10cbe2e514cb9c099bac64944bb6539e8
danielshulgin <danielshulgin@users.noreply.github.com>
2019-09-28 19:32:08 +0300

    FlashBackManager

---
+++ Assets/Scripts/Changable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Changable : MonoBehaviour
+{
+    public Sprite first;
+    public Sprite second;
+    public bool activeFirst = true;
+    public SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    public void Change()
+    {
+        if (activeFirst)
+        {
+            spriteRenderer.sprite = second;
+            activeFirst = false;
+        }
+        else
+        {
+            activeFirst = true;
+            spriteRenderer.sprite = first; 
+        }
+    }
+}
